9a3d4e2f73d57c6432fca47c18fc5971573298e4
Ayan Soni <ayansoni63@gmail.com>
2024-08-22 01:28:03 +0530

    Update merge_sort.js

--- scripts/merge_sort.js
+++ scripts/merge_sort.js
@@ -1,6 +1,6 @@
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   AYAN SONI
     
     *****************
 */
@@ -75,7 +75,7 @@ function merge_partition(start,end)
 
 /*
     *****************
-    DONE BY:-   TUMMALA KETHAN
+    DONE BY:-   AYAN SONI
     
     *****************
 */
